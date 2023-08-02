15c827aa7f3dfb4e2f6355130acbfc21176d0cb1
Alejandro Rodriguez <aperturegir@gmail.com>
2023-08-02 17:10:33 +0100

    solved lab

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -6,6 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+const randomBeer = punkAPI.getRandom()
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -21,5 +23,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then((beersFromApi) =>{
+    console.log(beersFromApi);
+
+    res.render("beers.hbs",{
+      beer : beersFromApi
+    })
+  })
+
+
+  .catch((error) =>{
+    console.log(error)
+  })
+})
+
+
+
+app.get('/random-beer', (req, res, next) =>{
+  punkAPI.getRandom()
+  .then((responseFromAPI)=>{
+    console.log(responseFromAPI)
+    // (beer[0].name)
+
+    res.render("random-beer.hbs",{
+      randomBeer : responseFromAPI
+    })
+  })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
